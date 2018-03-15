92836eabafcbe80bd01561341871470004986751
Brightcells <brightcells@gmail.com>
2018-03-15 23:04:48 +0800

    :art: Add Upper Param for CurtailUUID

--- django_curtail_uuid/curtail_uuid.py
+++ django_curtail_uuid/curtail_uuid.py
@@ -8,10 +8,12 @@ class _CurtailUUID:
     def __init__(self):
         self.length = hasattr(settings, 'CURTAIL_UUID_LENGTH') and settings.CURTAIL_UUID_LENGTH or 22
 
-    def uuid(self, model, field='uuid', length=None):
+    def uuid(self, model, field='uuid', length=None, upper=False):
         flag = True
         while flag:
             uuid = shortuuid.ShortUUID().random(length=length or self.length)
+            if upper:
+                uuid = uuid.upper()
             try:
                 model.objects.get(**{field: uuid})
             except model.DoesNotExist:
